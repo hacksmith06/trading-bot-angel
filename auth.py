eee83b5217a0dcc554490fd9ff3704776c1aecf0
hacksmith06 <singharpit0610@gmail.com>
2023-10-23 02:31:31 +0530

    created a function to use in other places

--- auth.py
+++ auth.py
@@ -1,43 +1,45 @@
+# auth.py
+
 # Import necessary packages
 from SmartApi import SmartConnect
 import pyotp
 import json
 
-# Load configuration from config.json
-with open('config-arpit.json', 'r') as file:
-    config = json.load(file)
 
-api_key = config["API_KEY"]
-client_id = config["CLIENT_ID"]
-password = config["PASSWORD"]
-totp_token = config["TOTP_TOKEN"]
+def authenticate():
+    # Load configuration from config.json
+    with open('config-arpit.json', 'r') as file:
+        config = json.load(file)
 
-# Initialize the SmartConnect object
-smart_api = SmartConnect(api_key)
+    api_key = config["API_KEY"]
+    client_id = config["CLIENT_ID"]
+    password = config["PASSWORD"]
+    totp_token = config["TOTP_TOKEN"]
 
-# Generate TOTP for authentication
-# get this from smart connect api website
-totp = pyotp.TOTP(totp_token).now()
+    # Initialize the SmartConnect object
+    smart_api = SmartConnect(api_key)
 
-# Authenticate and establish a session
-session_data = smart_api.generateSession(client_id, password, totp)
-print(session_data)
+    # Generate TOTP for authentication
+    totp = pyotp.TOTP(totp_token).now()
 
-# Extract authentication and refresh tokens
-auth_token = session_data['data']['jwtToken']
-refresh_token = session_data['data']['refreshToken']
+    # Authenticate and establish a session
+    session_data = smart_api.generateSession(client_id, password, totp)
 
-# Fetch the feed token
-feed_token = smart_api.getfeedToken()
-print(feed_token)
+    # Extract authentication and refresh tokens
+    auth_token = session_data['data']['jwtToken']
+    refresh_token = session_data['data']['refreshToken']
 
-# Fetch user profile
-user_profile = smart_api.getProfile(refresh_token)
-print(user_profile)
+    # Fetch the feed token
+    feed_token = smart_api.getfeedToken()
 
-# Generate a new token using the refresh token
-smart_api.generateToken(refresh_token)
+    # Generate a new token using the refresh token
+    smart_api.generateToken(refresh_token)
 
-# Extract and print available exchanges for the user
-exchanges = user_profile['data']['exchanges']
-print(exchanges)
+    return smart_api, auth_token, refresh_token, feed_token
+
+
+if __name__ == "__main__":
+    smart_api, auth_token, refresh_token, feed_token = authenticate()
+    print("Authentication Token:", auth_token)
+    print("Refresh Token:", refresh_token)
+    print("Feed Token:", feed_token)
