796b464d49bcd4b853c3c4e496104e0628c035a9
hacksmith06 <singharpit0610@gmail.com>
2023-10-23 02:03:13 +0530

    made changes to quicken the process

--- fetching_token_option_index.py
+++ fetching_token_option_index.py
@@ -1,27 +1,27 @@
 import requests
 
+# Fetch the JSON data from the URL and load it once
+url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
+response = requests.get(url)
+data = response.json()
 
-def get_token(name, expiry, strike):
-    # Fetch the JSON data from the URL
-    url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
-    response = requests.get(url)
-    data = response.json()
 
-    # Filter the data based on the given name, expiry, and strike
+def get_token(name, expiry, strike_and_option_type):
+    # Construct the symbol based on the input
+    symbol_pattern = f"{name}{expiry}{strike_and_option_type}"
+
+    # Filter the loaded data based on the constructed symbol
     for item in data:
-        if (item['name'].upper() == name.upper() and
-            item['expiry'] == expiry and
-            float(item['strike']) == float(strike)):
-            return item['token']
+        if item.get('symbol') == symbol_pattern:
+            return item.get('token')
     return None
 
 
-# Example usage
 name = "NIFTY"
-expiry = "18OCT2023"
-strike = "20000"
-token = get_token(name, expiry, strike)
+expiry = "26OCT23"
+strike_and_option_type = "20900PE"
+token = get_token(name, expiry, strike_and_option_type)
 if token:
-    print(f"Token for {name} with expiry {expiry} and strike {strike} is: {token}")
+    print(f"Token for {name} with expiry {expiry} and strike {strike_and_option_type} is: {token}")
 else:
-    print(f"No data found for {name} with expiry {expiry} and strike {strike}")
+    print(f"No data found for {name} with expiry {expiry} and strike {strike_and_option_type}")
