2ae1b7067292efee74e9efd4c58009149b3a99b9
hacksmith06 <singharpit0610@gmail.com>
2023-11-02 15:30:56 +0530

    random exp files

--- order.py
+++ order.py
@@ -5,34 +5,45 @@ from fetching_token_option_index import get_token
 # Get authenticated smart_api and tokens from auth.py
 smart_api, auth_token, refresh_token, feed_token = authenticate()
 
+# Define trade type: "EQUITY" or "OPTION"
+trade_type = "EQUITY"  # Change this to "OPTION" if you want to trade options
+
 # Fetch the stock token using the get_token function
 name = "NIFTY"
 expiry = "26OCT23"
 strike_and_option_type = "20900PE"
-stock_token = get_token(name, expiry, strike_and_option_type)
+
+if trade_type == "EQUITY":
+    stock_token = get_token(name)
+    tradingsymbol = name
+    producttype = "DELIVERY"
+else:
+    stock_token = get_token(name, expiry, strike_and_option_type)
+    tradingsymbol = f"{name}{expiry}{strike_and_option_type}"
+    producttype = "INTRADAY"
 
 # Place order for stock (or options)
 if stock_token:
     try:
         orderparams = {
             "variety": "ROBO",
-            "tradingsymbol": f"{name}{expiry}{strike_and_option_type}",
+            "tradingsymbol": tradingsymbol,
             "symboltoken": stock_token,
             "transactiontype": "BUY",
-            "exchange": "NFO",
-            "ordertype": "STOPLOSS_LIMIT",
-            "producttype": "INTRADAY",
+            "exchange": "NFO" if trade_type == "OPTION" else "NSE",
+            "ordertype": "MKT",
+            "producttype": producttype,
             "duration": "DAY",
-            "price": "OPTION_PRICE",
+            "price": "OPTION_PRICE" if trade_type == "OPTION" else "STOCK_PRICE",
             "squareoff": "0",
             "stoploss": "0",
-            "quantity": "1"  # Replace with the number of contracts you want to buy
+            "quantity": "1"  # Replace with the number of contracts or stocks you want to buy
         }
         orderId = smart_api.placeOrder(orderparams)
         print("The order id is: {}".format(orderId))
     except Exception as e:
         print("Order placement failed:", str(e))
 else:
-    print(f"No data found for {name} with expiry {expiry} and strike {strike_and_option_type}")
+    print(f"No data found for {name}")
 
 # ... [Any other operations you want to perform]
