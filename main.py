de20a181404a7f262a39cf37226005e795f3e94f
hacksmith06 <singharpit0610@gmail.com>
2023-11-02 15:30:28 +0530

    contains the main code for running algo

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
-"""
-Input Params:
-1. Trade Type - NIFTY
-2. Strike Price - 20,000
-3. Option Type - CE/PE
-4. Premium - 65.5
-
-System Flow:
-1. Create an entry in the database
-2. Create a order placement async task for each incoming order
-
-Ordering Placement System:
-1. Monitor the first 5 candles
-2. Calculate the high using the candles from 9.16 to 9.19
-3. Add 1 to the high price
-4. Place an order at 9.20 using the calculated price
-5. Update the status of the order in the order management system
-6. Create a order monitoring async task for each order after it's placement
-7. Do this for each order present in the ordering system (async)
-8. Orders will only be accepted between 9.00 to 9.15
-
-Order Monitor Async Task:
-# For each tick, evaluate whether we have hit the target price or have gone above it, if yes, square off
-# If we go below and hit the stoploss anyways we'll square off
+import requests
+from auth import authenticate
+import json
 
 
-Order Model:
-1. Trade Type - NIFTY
-2. Strike Price - 20,000
-3. Option Type - CE/PE
-4. Premium - 65.5
-5. Target Delta - 17
-6. Stoploss Delta - 7
-7. Status - CREATED, BOT_PENDING, MARKET_PENDING, MARKET_LIVE, MARKET_SQOFF, BOT_REJECTED
-8. Creation Datetime - Timestamp
-9. Exit Datetime - Timestamp
-10. Unique Order ID - System Generated
-"""
+# Read configuration from JSON file
+with open("config-vidya.json", "r") as config_file:
+    config = json.load(config_file)
 
 
+# Function to fetch the token for a given option
+def get_token_option(name, expiry, strike_and_option_type):
+    # Fetch the JSON data from the URL and load it once
+    url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
+    response = requests.get(url)
+    data = response.json()
 
+    # Construct the symbol based on the input
+    symbol_pattern = f"{name}{expiry}{strike_and_option_type}"
+
+    # Filter the loaded data based on the constructed symbol
+    for item in data:
+        if item.get('symbol') == symbol_pattern:
+            return item.get('token'), item.get('symbol')
+    return None, None
+
+
+# Function to place an order
+def place_order(name, expiry, strike_and_option_type):
+    # Get authenticated smart_api and tokens from auth.py
+    smart_api, auth_token, refresh_token, feed_token = authenticate()
+
+    # Fetch the stock token using the get_token_option function
+    token, symbol = get_token_option(name, expiry, strike_and_option_type)
+
+    if token:
+        try:
+            orderparams = {
+                "variety": "ROBO",
+                "tradingsymbol": symbol,
+                "symboltoken": token,
+                "transactiontype": "BUY",
+                "exchange": "NFO",
+                "ordertype": "LIMIT",
+                "producttype": "INTRADAY",
+                "duration": "DAY",
+                "price": "53",
+                "squareoff": "2",
+                "stoploss": "1",
+                "quantity": "15"
+            }
+            orderId = smart_api.placeOrder(orderparams)
+            print("The order id is: {}".format(orderId))
+        except Exception as e:
+            print("Order placement failed: {}".format(e.message))
+    else:
+        print("Token not found for the given option.")
+
+
+name = config["name"]
+expiry = config["expiry"]
+strike_and_option_type = config["strike_and_option_type"]
+
+place_order(name, expiry, strike_and_option_type)
